098dea50fb78433881867f99efd78f8a414a9d13
Filipe Cabaco <filipe@talkdesk.com>
2016-03-02 23:01:44 +0000

    added append and concat; concat needs tuning on huge list of small lists

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -2,27 +2,27 @@ defmodule ListOps do
 
   @spec count(list) :: non_neg_integer
   def count(l), do:    count(l, 0)
-  defp count([], r), do: r
-  defp count([_|t], r), do: count(t, r + 1)
+  defp count([], acc), do: acc
+  defp count([_|t], acc), do: count(t, acc + 1)
 
   @spec reverse(list) :: list
   def reverse(l), do: reverse(l, [])
-  defp reverse([], r), do: r
-  defp reverse([head|tail], r), do: reverse(tail, [head|r])
+  defp reverse([], acc), do: acc
+  defp reverse([head|tail], acc), do: reverse(tail, [head|acc])
   
   @spec map(list, (any -> any)) :: list
   def map(l, f), do: reverse(l) |> map(f, [])
-  defp map([], _, r) , do: r
-  defp map([h|t], f, r), do: map(t, f, [f.(h)|r]) 
+  defp map([], _, acc) , do: acc
+  defp map([h|t], f, acc), do: map(t, f, [f.(h)|acc]) 
   
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f), do: reverse(l) |> filter(f,[])
-  defp filter([], _, r), do: r  
-  defp filter([h|t], f,r) do
+  def filter(l, f), do: reverse(l) |> filter(f, [])
+  defp filter([], _, acc), do: acc  
+  defp filter([h|t], f, acc) do
     if apply(f,[h]) do
-      filter(t, f, [h|r])
+      filter(t, f, [h|acc])
     else
-      filter(t, f,r)
+      filter(t, f, acc)
     end
   end
 
@@ -32,12 +32,12 @@ defmodule ListOps do
   def reduce([h|t], acc, f), do: reduce(t, f.(h,acc), f)
  
   @spec append(list, list) :: list
-  def append(a, b) do
+  def append(a, b), do: reverse(a) |> do_append(b)
+  defp do_append(a, []), do: reverse(a)
+  defp do_append([] , b), do: b
+  defp do_append(a, [h|t]), do: do_append([h|a],t) 
 
-  end
-  
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
+  def concat(ll), do: reduce(ll,[],&(append(&2,&1)))
 
-  end
 end
